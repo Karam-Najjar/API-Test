7f40ea847b32582b4308254513415e6bd8ec8598
Karam-Najjar <karam.najjar1@gmail.com>
2024-03-05 14:37:00 +0300

    Feature/Add summary to the user form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,13 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
 import { AuthComponent } from './auth/components/auth.component';
 import { HeaderComponent } from './applications/layouts/header/header.component';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, PageNotFoundComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     AppRoutingModule,
     BrowserModule,
